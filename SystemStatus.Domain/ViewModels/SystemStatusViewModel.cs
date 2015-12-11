968a5e3947188d2564100cc979e3a90885b803ad
David Hathaway <david.hathaway@capita..o.uk>
2015-12-11 15:28:05 +0000

    missing files (see last)

---
+++ SystemStatus.Domain/ViewModels/SystemStatusViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SystemStatus.Domain.ViewModels
+{
+    public class SystemStatusViewModel
+    {
+        public int SystemGroupID { get; set; }
+        public string Name { get; set; }
+        public AppStatus[] AppStatuses { get; set; }
+        public string DrillDownUrl { get; set; }
+    }
+}
